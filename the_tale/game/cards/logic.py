c50d3687880528161ce78b33fd545e7a7d75cc50
Aleksey Yeletsky <a.eletsky@gmail.com>
2011-10-08 18:04:06 +0000

    gh-23 добавить celery в проект и вынести логику в воркеры (remove cards application)

--- the_tale/game/cards/logic.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .models import Card
-from .prototypes import get_card_by_model
-
-def get_angel_deck(angel_id):
-    cards = Card.objects.filter(angel=angel_id)
-    deck = [get_card_by_model(card) for card in cards]
-    return deck
--- the_tale/game/cards/urls.py
+++
@@ -1,5 +0,0 @@
-from django_next.views.dispatcher import resource_patterns
-
-from .views import CardsResource
-
-urlpatterns = resource_patterns(CardsResource)
