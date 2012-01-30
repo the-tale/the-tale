7d892ad23276f9d626c68a505a208d914120a9d2
Aleksey Yeletsky <a.eletsky@gmail.com>
2012-01-30 07:13:09 +0000

    gh-47 Подготовить проект к автоматическому деплою и автономной работе

--- the_tale/scripts/workers.sh
+++ the_tale/scripts/workers.sh
@@ -9,18 +9,18 @@ LOGS_DIR="${HOME}/logs"
 case "$1" in
 start)
 echo "Starting game workers..."
-. ../../env/bin/activate && nohup python -u ./manage.py game_supervisor 2>&1 1>${LOGS_DIR}/game_supervisor.log &
-. ../../env/bin/activate && nohup python -u ./manage.py game_logic 2>&1 1>${LOGS_DIR}/game_logic.log &
 . ../../env/bin/activate && nohup python -u ./manage.py game_highlevel 2>&1 1>${LOGS_DIR}/game_highlevel.log &
+. ../../env/bin/activate && nohup python -u ./manage.py game_logic 2>&1 1>${LOGS_DIR}/game_logic.log &
+. ../../env/bin/activate && nohup python -u ./manage.py game_supervisor 2>&1 1>${LOGS_DIR}/game_supervisor.log &
 . ../../env/bin/activate && nohup python -u ./manage.py game_turns_loop 2>&1 1>${LOGS_DIR}/game_turns_loop.log &
 ;;
 
 stop)
 echo "Stop game workers..."
+pkill -f game_turns_loop
 pkill -f game_supervisor
 pkill -f game_logic
 pkill -f game_highlevel
-pkill -f game_turns_loop
 ;;
 
 status)
