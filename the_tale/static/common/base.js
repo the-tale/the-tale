bd965437d949d6da2927921342e0117aa1a5a967
Aleksey Yeletsky <a.eletsky@gmail.com>
2012-02-03 07:19:46 +0000

    gh-51 Создать форум

--- the_tale/static/common/base.js
+++ the_tale/static/common/base.js
@@ -36,6 +36,15 @@ pgf.base.RenderTemplateList = function(selector, data, newElementCallback, param
 
 };
 
+pgf.base.AutoFormatTime = function() {
+    jQuery('.pgf-format-time').each(function(i, v){
+        var el = jQuery(v);
+        var timestamp = parseInt(el.data('timestamp'), 10) * 1000;
+        var text = Globalize.format( new Date(timestamp), "f" ) + ' UTC';
+        el.text(text);
+    });
+};
+
 pgf.base.AddPreview = function(blockSelector, contentSourceSelector, previewUrl) {
 
     var block = jQuery(blockSelector);
