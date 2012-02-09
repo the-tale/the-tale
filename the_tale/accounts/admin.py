7becb2fdedf08a1b4efadaee4fe0e3e5fc4be743
Aleksey Yeletsky <a.eletsky@gmail.com>
2012-02-09 23:02:43 +0000

    gh-66 Добавить или начать отображать время последнего входа пользователя в игру

--- the_tale/accounts/admin.py
+++ the_tale/accounts/admin.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
 from .models import Account
 
 class AccountAdmin(admin.ModelAdmin):
     list_display = ('id', 'user')
 
+class UserAdmin(DjangoUserAdmin):
+    list_display = ('id', 'username', 'is_staff', 'last_login')
+
 admin.site.register(Account, AccountAdmin)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
