b5b5876212118c2bd66536a2bd1604c0672597c6
Aleksey Yeletsky <a.eletsky@gmail.com>
2012-02-11 07:34:20 +0000

    gh-61 организовать бэкапы базы

---
+++ the_tale/portal/conf.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+from dext.utils.app_settings import app_settings
+
+portal_settings = app_settings('PORTAL', 
+                               DUMP_EMAIL='a.eletsky@gmail.com')
+
---
+++ the_tale/portal/management/commands/portal_dump.py
@@ -0,0 +1,60 @@
+# coding: utf-8
+import os
+import shutil
+import datetime
+import tempfile
+import subprocess
+
+from django.core.management.base import BaseCommand
+from django.conf import settings as project_settings
+from django.core.mail import EmailMessage
+
+from portal.conf import portal_settings
+
+class Command(BaseCommand):
+
+    help = 'dump all dymamic portal data and send to email from settings.DUMP_EMAIL'
+
+    requires_model_validation = False
+
+    def handle(self, *args, **options):
+
+        tmp_dir = tempfile.mkdtemp()
+
+        try:
+            timestamp_string = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+        
+            backup_dir = os.path.join(tmp_dir, timestamp_string)
+
+            os.mkdir(backup_dir)
+
+            for db_name, db_data in project_settings.DATABASES.items():
+                backup_file_name = os.path.join(backup_dir, db_data['NAME'] + '.sql')
+                print 'dump: %s' % db_name
+                cmd = 'pg_dump -U "%(db_user)s" "%(db_name)s" > "%(file_name)s"' % {'db_user': db_data['USER'],
+                                                                                    'db_name': db_data['NAME'],
+                                                                                    'file_name': backup_file_name}
+                subprocess.call(cmd, shell=True)
+
+
+            print 'make archive'
+
+            archive_file = shutil.make_archive(os.path.join(tmp_dir, timestamp_string),
+                                               format='gztar',
+                                               root_dir=tmp_dir,
+                                               base_dir=timestamp_string)
+
+            print 'send email to %s' % portal_settings.DUMP_EMAIL
+
+            email = EmailMessage('[BACKUP][the-tale.org] %s' % timestamp_string, 
+                                 'backup file for %s' % timestamp_string,
+                                 'no-reply@the-tale.org',
+                                 [portal_settings.DUMP_EMAIL])
+            email.attach_file(archive_file)
+            email.send()
+
+        except:
+            raise
+        finally:
+            shutil.rmtree(tmp_dir)
+        
