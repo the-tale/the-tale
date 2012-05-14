753c2f90f906fa54f93fe580d899f072482875f8
Tiendil <a.eletsky@gmail.com>
2012-05-14 21:51:42 +0300

    gh-107 окончательно автоматизировать поднятие окружения и обновление проекта

--- the_tale/portal/management/commands/portal_postupdate_operations.py
+++ the_tale/portal/management/commands/portal_postupdate_operations.py
@@ -4,6 +4,8 @@ import subprocess
 
 from django.core.management.base import BaseCommand
 
+from game.models import Time
+
 class Command(BaseCommand):
 
     help = 'do post update operations'
@@ -11,6 +13,24 @@ class Command(BaseCommand):
     requires_model_validation = False
 
     def handle(self, *args, **options):
+
+        if not Time.objects.all().exists():
+            print
+            print 'CREATE TIME OBJECT'
+            print
+            Time.objects.create()
+
+            print
+            print 'CREATE TEST MAP'
+            print
+            subprocess.call(['./manage.py', 'map_create_test_map'])
+
+            print
+            print 'CREATE_PERSONS'
+            print
+
+            subprocess.call(['./manage.py', 'places_sync'])
+
 
         print
         print 'CLEAR TEXTGEN TEXTS'
