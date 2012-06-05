bc59db972d9cae21065f123e466fd5fbb21e9cbe
Tiendil <a.eletsky@gmail.com>
2012-06-05 16:26:20 +0300

    gh-108 исправить слова

--- the_tale/game/tests.py
+++ the_tale/game/tests.py
@@ -15,6 +15,8 @@ from textgen.conf import textgen_settings
 # from game.artifacts import logic as artifacts_logic
 from game.text_generation import get_dictionary
 
+from game import expected_values
+
 morph = pymorphy.get_morph(textgen_settings.PYMORPHY_DICTS_DIRECTORY)
 
 class GameTest(TestCase):
@@ -31,9 +33,6 @@ class GameTest(TestCase):
 
     def test_vocabulary_consistency(self):
 
-        # mobs_logic.import_texts_into_database(morph)
-        # artifacts_logic.import_texts_into_database(morph)
-
         dictionary = get_dictionary()
 
         for mob_record in MobsDatabase.storage().data.values():
@@ -43,3 +42,11 @@ class GameTest(TestCase):
         for artifact_record in ArtifactsDatabase.storage().data.values():
             word = dictionary.get_word(artifact_record.normalized_name)
             self.assertTrue(not isinstance(word, words.Fake))
+
+    def test_items_consitency(self):
+        real_artifacts_list = set(ArtifactsDatabase.storage().data.keys())
+        self.assertEqual(real_artifacts_list, expected_values.artifacts_list)
+
+    def test_mobs_consitency(self):
+        real_mobs_list = set(MobsDatabase.storage().data.keys())
+        self.assertEqual(real_mobs_list, expected_values.mobs_list)
