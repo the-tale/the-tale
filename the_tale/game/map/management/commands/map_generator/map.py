896b148f39047c98bc755cd75d31c305ef4ca266
Tiendil <a.eletsky@gmail.com>
2012-06-27 20:15:49 +0300

    gh-149 сделать нормальную наполняемость карты (т.е. что бы небыло дефолтной травы)

--- the_tale/game/map/management/commands/map_generator/map.py
+++ the_tale/game/map/management/commands/map_generator/map.py
@@ -6,23 +6,33 @@ from .road_roller import roll_road
 class MapGeneratorException(Exception): pass
 
 class Cell(object):
-    
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.terrains = {}
         self.terrain = TERRAIN.GRASS
 
+        self.nearest_place_terrain = TERRAIN.GRASS
+        self.nearest_place_distance = -1
+
+    def update_nearest_place(self, center_x, center_y, terrain):
+        distance = (center_x-self.x)**2 + (center_y-self.y)**2
+        if (self.nearest_place_distance == -1 or distance < self.nearest_place_distance):
+            self.nearest_place_distance = distance
+            self.nearest_place_terrain = terrain
+
     def update_terrain(self, terrain, power):
         self.terrains[terrain] = self.terrains.get(terrain, 0) + power
 
     def choose_terrain(self):
+        self.terrain = self.nearest_place_terrain
         cur_power = -1
         for terrain, power in self.terrains.items():
             if power > cur_power:
                 cur_power = power
                 self.terrain = terrain
-            
+
     def __repr__(self):
         return 'Cell(%i, %i)' % (self.x, self.y)
 
@@ -32,7 +42,7 @@ class Map(object):
     def __init__(self, w, h):
         self.w = w
         self.h = h
-        
+
         self.map = [ [Cell(x, y) for x in xrange(w)] for y in xrange(h)]
 
         self.places = {}
@@ -45,6 +55,7 @@ class Map(object):
         self.places[place.id] = place
 
         self.update_terrain(place.x, place.y, place.significance, place.terrain)
+        self.update_nearest_places(place.x, place.y, place.terrain)
 
     def add_road(self, road):
         if road.key in self.roads:
@@ -52,7 +63,7 @@ class Map(object):
         self.roads[road.key] = road
 
     def update_terrain(self, center_x, center_y, significance, terrain):
-        
+
         sig_radius = int(significance)
 
         for y in xrange(center_y - sig_radius, center_y + sig_radius):
@@ -60,6 +71,11 @@ class Map(object):
                 distance = math.sqrt( math.pow(y - center_y, 2) + math.pow(x - center_x, 2))
                 if 0 <= y < self.h and 0 <= x < self.w:
                     self.map[y][x].update_terrain(terrain, max(0, sig_radius - distance) )
+
+    def update_nearest_places(self, center_x, center_y, terrain):
+        for row in self.map:
+            for cell in row:
+                cell.update_nearest_place(center_x, center_y, terrain)
 
     def prepair_terrain(self):
         for row in self.map:
@@ -71,7 +87,7 @@ class Map(object):
 
     def pave_ways(self):
         for road in self.roads.values():
-            roll_road(road, 
+            roll_road(road,
                       self.places[road.point_1].x,
                       self.places[road.point_1].y,
                       self.places[road.point_2].x,
@@ -97,5 +113,4 @@ class Map(object):
 
     def print_terrain_map(self):
         for row in self.get_terrain_map():
-            print ''.join(row)     
-
+            print ''.join(row)
