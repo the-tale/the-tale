3768cd0f5daa6b798b0944177297d68695c8016f
Tiendil <a.eletsky@gmail.com>
2012-06-28 09:24:39 +0300

    gh-156 не генерировать артефакты +0 силы

--- the_tale/game/balance/formulas.py
+++ the_tale/game/balance/formulas.py
@@ -44,7 +44,7 @@ def power_to_artifact(lvl): return power_to_lvl(lvl) / c.EQUIP_SLOTS_NUMBER
 # функция, для получения случайного значения силы артефакта
 def power_to_artifact_randomized(lvl):
     base_power = power_to_artifact(lvl)
-    return random.randint(max(base_power-c.ARTIFACT_POWER_DELTA, 0), base_power+c.ARTIFACT_POWER_DELTA)
+    return random.randint(max(base_power-c.ARTIFACT_POWER_DELTA, 1), base_power+c.ARTIFACT_POWER_DELTA)
 
 
 # Предполагаем, что мобы различаются по инициативе (скорости), здоровью и урону. Каждый из этих параметров высчитывается как процент от среднего (ожидаемого) значения.
