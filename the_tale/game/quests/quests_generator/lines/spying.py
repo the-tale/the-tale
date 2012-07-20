749014342123e26cec58f13bc797f2172d7141d0
Tiendil <a.eletsky@gmail.com>
2012-07-20 12:44:15 +0300

    gh-216 в случае если я прошпионю то как квест гиверю так и объекту шпионажа -1. а если не прошпионю то обоим +1

--- the_tale/game/quests/quests_generator/lines/spying.py
+++ the_tale/game/quests/quests_generator/lines/spying.py
@@ -34,12 +34,12 @@ class Spying(Quest):
         bad_line_1 = Line(sequence=[ cmd.MoveNear(place=self.env_local.place_end, back=True, event=EVENTS.MOVE_NEAR),
                                      cmd.GetReward(person=self.env_local.person_end, event=EVENTS.GET_REWARD),
                                      cmd.GivePower(person=self.env_local.person_start, power=-1, event=EVENTS.BAD_GIVE_POWER),
-                                     cmd.GivePower(person=self.env_local.person_end, power=-1, event=EVENTS.BAD_GIVE_POWER)])
+                                     cmd.GivePower(person=self.env_local.person_end, power=1, event=EVENTS.BAD_GIVE_POWER)])
 
         good_line_2 = Line(sequence=[cmd.MoveNear(place=self.env_local.place_end, back=True, event=EVENTS.MOVE_NEAR),
                                      cmd.GetReward(person=self.env_local.person_end, event=EVENTS.GET_REWARD),
                                      cmd.GivePower(person=self.env_local.person_start, power=1, event=EVENTS.GOOD_GIVE_POWER),
-                                     cmd.GivePower(person=self.env_local.person_end, power=1, event=EVENTS.GOOD_GIVE_POWER)])
+                                     cmd.GivePower(person=self.env_local.person_end, power=-1, event=EVENTS.GOOD_GIVE_POWER)])
 
         main_line = Line(sequence=[cmd.Move(place=self.env_local.place_end, event=EVENTS.MOVE_TO_QUEST),
                                    cmd.MoveNear(place=self.env_local.place_end, back=False, event=EVENTS.MOVE_NEAR),
