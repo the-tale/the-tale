7cc7ec583a38e347a30cdc0bcf2495679dc1adf6
Tiendil <a.eletsky@gmail.com>
2012-08-12 14:43:22 +0300

    gh-68 Доработки по автоматическому деплою

---
+++ the_tale/portal/management/commands/portal_devops.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+
+import subprocess
+
+from optparse import make_option
+
+from django.core.management.base import BaseCommand
+
+from meta_config import meta_config
+
+FABFILE = '/home/tie/repos/mine/devops/the_tale'
+SETUP_HOST = "tie@192.168.1.104"
+UPDATE_HOST = "the-tale@192.168.1.104"
+
+class Command(BaseCommand):
+
+    help = 'devops commands'
+
+    requires_model_validation = False
+
+    option_list = BaseCommand.option_list + ( make_option('-c', '--command',
+                                                          action='store',
+                                                          type=str,
+                                                          dest='command',
+                                                          help='command'),
+                                            )
+
+
+    def handle(self, *args, **options):
+
+        command = options['command']
+
+        if command == 'setup':
+            subprocess.call(['fab', '-f', FABFILE, 'setup:host=%s' % SETUP_HOST])
+
+        elif command == 'update':
+            subprocess.call(['fab', '-f', FABFILE, 'update:static_data_version=%s,host=%s' % (meta_config.static_data_version, UPDATE_HOST)])
+        else:
+
+            print 'unknown command'
