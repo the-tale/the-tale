a67ca8e6e4c7b53bd19161406fad9d238f6d6314
Tiendil <a.eletsky@gmail.com>
2012-08-12 13:37:29 +0300

    gh-68 Доработки по автоматическому деплою

--- the_tale/game/heroes/migrations/0025_auto__add_field_hero_pref_place__add_field_hero_pref_friend__add_field.py
+++ the_tale/game/heroes/migrations/0025_auto__add_field_hero_pref_place__add_field_hero_pref_friend__add_field.py
@@ -7,6 +7,11 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("persons", "0001_initial"),
+        ("places", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding field 'Hero.pref_place'
         db.add_column('heroes_hero', 'pref_place',
