f69382d1e4231e7788652b17d404f6a08849cf0c
Tiendil <a.eletsky@gmail.com>
2012-09-13 21:55:43 +0300

    gh-228 Названия разделов в меню (главном и в гайде) брать из описания секции cms, а не хардкодить

--- the_tale/cms/jinjaglobals.py
+++ the_tale/cms/jinjaglobals.py
@@ -2,7 +2,15 @@
 from dext.jinja2.decorators import jinjaglobal
 
 from cms.models import Page
+from cms.conf import cms_settings
+
+SECTIONS_DICT = dict( (section.id, section) for section in cms_settings.SECTIONS)
 
 @jinjaglobal
 def get_cms_section_pages(section, *args, **kwargs):
     return Page.objects.filter(section=section, active=True).order_by('order')
+
+
+@jinjaglobal
+def get_cms_section_info(section):
+    return SECTIONS_DICT[section]
