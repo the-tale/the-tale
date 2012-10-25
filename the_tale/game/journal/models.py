46b41642bfc6f3db297e8db01e76068136a9a6ae
Tiendil <a.eletsky@gmail.com>
2012-10-25 10:14:49 +0300

    gh-351 удалить приложение journal

--- the_tale/game/journal/models.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-from django.db import models
-
-class Phrase(models.Model):
-
-    type = models.CharField(null=False, max_length=64, db_index=True)
-    
-    template = models.TextField(null=False, default='')
