cadcd040cfa8c60763aa538f89f892f5644be0b3
Tiendil <a.eletsky@gmail.com>
2012-11-29 08:04:18 +0300

    gh-413 добавить страницу о городах в гайд

--- the_tale/guide/tests/requests_tests.py
+++ the_tale/guide/tests/requests_tests.py
@@ -1,6 +1,9 @@
 # coding: utf-8
-from django.test import TestCase, client
+
 from django.core.urlresolvers import reverse
+from django.test import client
+
+from common.utils.testcase import TestCase
 
 
 class TestRequests(TestCase):
@@ -8,18 +11,26 @@ class TestRequests(TestCase):
     def setUp(self):
         self.client = client.Client()
 
+    def test_index(self):
+        self.check_redirect(reverse('guide:'), reverse('guide:game'))
+
     def test_registration(self):
-        response = self.client.get(reverse('guide:registration'))
-        self.assertEqual(response.status_code, 200)
+        self.check_html_ok(self.client.get(reverse('guide:registration')))
 
     def test_game(self):
-        response = self.client.get(reverse('guide:game'))
-        self.assertEqual(response.status_code, 200)
+        self.check_html_ok(self.client.get(reverse('guide:game')))
 
     def test_might(self):
-        response = self.client.get(reverse('guide:might'))
-        self.assertEqual(response.status_code, 200)
+        self.check_html_ok(self.client.get(reverse('guide:might')))
+
+    def test_cities(self):
+        self.check_html_ok(self.client.get(reverse('guide:cities')))
+
+    def test_politics(self):
+        self.check_html_ok(self.client.get(reverse('guide:politics')))
 
     def test_hero_abilities(self):
-        response = self.client.get(reverse('guide:hero-abilities'))
-        self.assertEqual(response.status_code, 200)
+        self.check_html_ok(self.client.get(reverse('guide:hero-abilities')))
+
+    def test_hero_preferences(self):
+        self.check_html_ok(self.client.get(reverse('guide:hero-preferences')))
