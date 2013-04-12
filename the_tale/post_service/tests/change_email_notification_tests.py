6f99cbd7d201f164bfa878d2506c054ef3404c60
Tiendil <a.eletsky@gmail.com>
2013-04-12 14:41:08 +0300

    gh-630 тест на отправку email с нотификацией о смене почты для быстрого аккаунта

--- the_tale/post_service/tests/change_email_notification_tests.py
+++ the_tale/post_service/tests/change_email_notification_tests.py
@@ -22,23 +22,45 @@ class ChangeEmailNotificationTests(testcase.TestCase):
         register_user('user_1', 'user_1@test.com', '111111')
         self.account = AccountPrototype.get_by_nick('user_1')
 
-        self.task = ChangeCredentialsTaskPrototype.create(account=self.account,
-                                                          new_email='user_2@test.com',
-                                                          new_password='111111',
-                                                          new_nick='user_2')
-        self.task.request_email_confirmation()
 
-        self.message = MessagePrototype.get_priority_message()
+    def create_task_and_message(self, account, new_nick):
+        task = ChangeCredentialsTaskPrototype.create(account=account,
+                                                     new_email='%s@test.com' % new_nick,
+                                                     new_password='111111',
+                                                     new_nick=new_nick)
+        task.request_email_confirmation()
+
+        return task, MessagePrototype.get_priority_message()
 
     def test_create_message(self):
+        self.create_task_and_message(self.account, 'user_1_new')
         self.assertEqual(Message.objects.all().count(), 1)
 
     def test_mail_send(self):
+        task, message = self.create_task_and_message(self.account, 'user_1_new')
         self.assertEqual(len(mail.outbox), 0)
-        self.message.process()
-        self.assertTrue(self.message.state._is_PROCESSED)
+        message.process()
+        self.assertTrue(message.state._is_PROCESSED)
         self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].to, [self.account.email])
+        self.assertEqual(mail.outbox[0].to, ['user_1_new@test.com'])
 
-        self.assertTrue(self.task.uuid in mail.outbox[0].body)
-        self.assertTrue(self.task.uuid in mail.outbox[0].alternatives[0][0])
+        self.assertTrue(task.uuid in mail.outbox[0].body)
+        self.assertTrue(task.uuid in mail.outbox[0].alternatives[0][0])
+
+    def test_mail_send_for_fast_account(self):
+        register_user('user_2')
+        account = AccountPrototype.get_by_nick('user_2')
+
+        task, message = self.create_task_and_message(account, 'user_2_new')
+
+        self.assertEqual(len(mail.outbox), 0)
+
+        message = MessagePrototype.get_priority_message()
+        message.process()
+
+        self.assertTrue(message.state._is_PROCESSED)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].to, ['user_2_new@test.com'])
+
+        self.assertTrue(task.uuid in mail.outbox[0].body)
+        self.assertTrue(task.uuid in mail.outbox[0].alternatives[0][0])
