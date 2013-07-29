25b5a26f63f06bfa9ae84e5a0c7aa26a04a09914
Tiendil <a.eletsky@gmail.com>
2013-07-29 18:27:27 +0300

    gh-797 увеличить вероятность выбора прекращения безделья при помощи

--- the_tale/game/abilities/relations.py
+++ the_tale/game/abilities/relations.py
@@ -10,6 +10,6 @@ class HELP_CHOICES(DjangoEnum):
     _records = ( ('HEAL', 0, u'лечение', 4),
                  ('TELEPORT', 1, u'телепорт', 4),
                  ('LIGHTING', 2, u'молния', 4),
-                 ('START_QUEST', 3, u'начало задания', 4),
+                 ('START_QUEST', 3, u'начало задания', 20),
                  ('MONEY', 4, u'деньги', 1),
-                 ('RESURRECT', 5, u'воскрешение', 10) )
+                 ('RESURRECT', 5, u'воскрешение', 20) )
