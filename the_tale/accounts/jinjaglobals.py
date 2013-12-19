7804b8018867e18e65ab9d6907a7659ce739e076
Tiendil <a.eletsky@gmail.com>
2013-12-19 12:41:58 +0300

    gh-995 Кнопка «пожаловаться» на форуме

--- the_tale/accounts/jinjaglobals.py
+++ the_tale/accounts/jinjaglobals.py
@@ -5,6 +5,8 @@ import jinja2
 from dext.jinja2.decorators import jinjaglobal
 
 from the_tale.accounts import logic
+from the_tale.accounts.conf import accounts_settings
+
 
 @jinjaglobal
 def login_page_url(next_url='/'):
@@ -17,3 +19,7 @@ def login_url(next_url='/'):
 @jinjaglobal
 def logout_url():
     return jinja2.Markup(logic.logout_url())
+
+@jinjaglobal
+def forum_complaint_theme():
+    return accounts_settings.FORUM_COMPLAINT_THEME
