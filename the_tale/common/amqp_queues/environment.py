5dcaa59700db02eab25a041dd1c22861da794450
Tiendil <a.eletsky@gmail.com>
2014-07-12 21:16:11 +0300

    gh-1206 перенос в Dext части функционала

--- the_tale/common/amqp_queues/environment.py
+++ the_tale/common/amqp_queues/environment.py
@@ -1,6 +1,4 @@
 # coding: utf-8
-from django.utils.log import getLogger
-logger = getLogger('the-tale.workers.game_supervisor')
 
 class BaseEnvironment(object):
 
@@ -9,13 +7,8 @@ class BaseEnvironment(object):
 
     def __getattribute__(self, name):
         if not super(BaseEnvironment, self).__getattribute__('initialized'):
-            import sys
-            if 'game_supervisor' in sys.argv:
-                logger.error('ENVIRONMENT INITIALIZATION > ')
             super(BaseEnvironment, self).__getattribute__('initialize')()
             self.initialized = True
-            if 'game_supervisor' in sys.argv:
-                logger.error('ENVIRONMENT INITIALIZATION < %r' % self.initialized)
         return super(BaseEnvironment, self).__getattribute__(name)
 
     def initialize(self):
