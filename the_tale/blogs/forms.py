84a13bc6eda13a2d2c3ebd66573aac8a448ba038
Tiendil <a.eletsky@gmail.com>
2014-07-16 20:55:18 +0300

    gh-1209 ограничить минимальный размер поста в фольклоре

--- the_tale/blogs/forms.py
+++ the_tale/blogs/forms.py
@@ -8,6 +8,7 @@ from the_tale.blogs.models import Post
 
 
 class PostForm(forms.Form):
+    MIN_TEXT_LENGTH = 1000
 
     caption = fields.CharField(label=u'Название', max_length=Post.CAPTION_MAX_LENGTH, min_length=Post.CAPTION_MIN_LENGTH)
-    text = bbcode.BBField(label=u'Текст')
+    text = bbcode.BBField(label=u'Текст', min_length=MIN_TEXT_LENGTH)
