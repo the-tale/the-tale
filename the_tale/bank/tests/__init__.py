aedec73add731439369d0262964c6243a0cf4ed7
Tiendil <a.eletsky@gmail.com>
2014-07-20 19:01:17 +0300

    gh-1101 один из инвойсов, замороженных во время падения логики игры, не обработался

--- the_tale/bank/tests/__init__.py
+++ the_tale/bank/tests/__init__.py
@@ -1,6 +1,1 @@
 # coding: utf-8
-
-from the_tale.bank.tests.prototypes_tests import *
-from the_tale.bank.tests.bank_processor_tests import *
-from the_tale.bank.tests.transaction_tests import *
-from the_tale.bank.tests.logic_tests import *
