4bbba0afb60f13f8a6a78d7caab8352f394d3fc5
Tiendil <a.eletsky@gmail.com>
2014-07-30 17:59:00 +0300

    gh-1218 Ограничить минимальный размер описания закона

--- the_tale/game/bills/forms.py
+++ the_tale/game/bills/forms.py
@@ -9,9 +9,10 @@ from the_tale.game.bills.relations import BILL_DURATION
 
 
 class BaseUserForm(forms.Form):
+    RATIONALE_MIN_LENGTH = 250
 
     caption = fields.CharField(label=u'Название закона', max_length=Bill.CAPTION_MAX_LENGTH, min_length=Bill.CAPTION_MIN_LENGTH)
-    rationale = bbcode.BBField(label=u'Обоснование')
+    rationale = bbcode.BBField(label=u'Обоснование', min_length=RATIONALE_MIN_LENGTH)
     duration = fields.TypedChoiceField(label=u'Время действия', choices=BILL_DURATION.choices(), coerce=BILL_DURATION.get_from_name, required=False)
 
     def clean_duration(self):
