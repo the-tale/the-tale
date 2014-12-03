ff991b5cb9f0a429e9431c31d08c44316f41e560
Tiendil <a.eletsky@gmail.com>
2014-12-03 14:04:56 +0300

    gh-1322 Добавить ссылки на статистику

--- the_tale/portal/jinjaglobals.py
+++ the_tale/portal/jinjaglobals.py
@@ -18,3 +18,21 @@ def players_projects_url():
 @jinjaglobal
 def get_edition_number():
     return (datetime.datetime.now() - portal_settings.FIRST_EDITION_DATE).days + 1
+
+
+
+@jinjaglobal
+def standy_statistics_url():
+    return 'http://standy.github.io/the-tale-stats/'
+
+@jinjaglobal
+def standy_statistics_place_url(place_id):
+    return 'http://standy.github.io/the-tale-stats/#!place/%d' % place_id
+
+@jinjaglobal
+def standy_statistics_clan_url(clan_id):
+    return 'http://standy.github.io/the-tale-stats/#!clan/%d' % clan_id
+
+@jinjaglobal
+def standy_statistics_account_url(account_id):
+    return 'http://standy.github.io/the-tale-stats/#!player/%d' % account_id
