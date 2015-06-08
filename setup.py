a0601820e7ce8006bfa837790c22fe040c28f632
Tiendil <a.eletsky@gmail.com>
2015-06-08 14:44:46 +0300

    gh-1653 setup.py & requirements.txt

--- setup.py
+++ setup.py
@@ -1,18 +1,60 @@
 # coding: utf-8
+import re
+import json
 import setuptools
 
-# find ./ | grep -v \.html'' | grep -v '\.py' | grep -v '\.pyc' | grep -v '\.js' | grep -v '\.css' | grep -v gitignore | grep -v '\.png' | grep -v '\.gif' | grep -v '\.otf' | grep -v '\.ttf' | grep -v '\.jpg' | grep -v '\.sh'
+
+
+with open('./the_tale/meta_config.json') as f:
+    config = json.loads(f.read())
+    VERSION = re.search('((\d+\.?)+)', config['version']).group(0)
 
 
 setuptools.setup(
-    name = 'TheTale',
-    version = '0.1.0',
-    author = 'Aleksey Yeletsky',
-    author_email = 'a.eletsky@gmail.com',
-    packages = setuptools.find_packages(),
-    url = 'https://github.com/Tiendil/the-tale',
-    license = 'LICENSE',
-    description = "MMO with indirect control characters",
-    long_description = open('README').read(),
-    include_package_data = True # setuptools-git MUST be installed
-)
+    name='TheTale',
+    version=VERSION,
+    description='Zero Player Game with indirect character controll',
+    long_description = open('README.rst').read(),
+    url='https://github.com/Tiendil/the-tale',
+    author='Aleksey Yeletsky <Tiendil>',
+    author_email='a.eletsky@gmail.com',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+
+        'Topic :: Games/Entertainment',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+
+        'License :: OSI Approved :: BSD License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+
+        'Natural Language :: English',
+        'Natural Language :: Russian'],
+    keywords=['gamedev', 'the-tale', 'game development', 'zpg', 'zero player game'],
+    packages=setuptools.find_packages(),
+    install_requires=['Django>=1.8.2',
+                      'psycopg2>=2.4.5',
+                      'kombu>=2.3.2',
+                      'postmarkup>=1.2.0',
+                      'markdown>=2.2.0',
+                      'xlrd>=0.8.0',
+                      'mock>=1.0b1',
+                      'pylibmc>=1.2.3',
+                      'MarkupSafe>=0.15',
+                      'boto>=2.15.0',
+
+                      'pynames==0.2.0',
+                      'utg==0.1.0',
+                      'rels==0.2.2.2',
+
+                      'dext==0.2.0',
+                      'deworld==0.2.0',
+                      'questgen==0.2.0'],
+
+    include_package_data=True,
+    test_suite = 'tests',
+    )
