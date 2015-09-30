de8ad2ceda2ce5117562d52d32c1d9954ce34633
Tiendil <a.eletsky@gmail.com>
2015-09-30 14:43:11 +0300

    gh-1752 Добавить описание конфигурации почты

--- the_tale/post_service/conf.py
+++ the_tale/post_service/conf.py
@@ -4,8 +4,14 @@
 from dext.common.utils.app_settings import app_settings
 
 post_service_settings = app_settings('POST_SERVICE',
-                                     MESSAGE_SENDER_DELAY=60,
-                                     ENABLE_MESSAGE_SENDER=True,
-                                     SETTINGS_ALLOWED_KEY='post service allowed',
-                                     SETTINGS_FORCE_ALLOWED_KEY='post service force allowed',
-                                     MESSAGE_LIVE_TIME=2*24*60*60)
+                                     MESSAGE_SENDER_DELAY=60,                                 # Задержка между проверкой наличия новых собщений
+                                     ENABLE_MESSAGE_SENDER=True,                              # Включить/выключить фонового рабочего
+                                     SETTINGS_ALLOWED_KEY='post service allowed',             # Идентификатор настройки в таблице settings в админке Django
+                                                                                              # используется как дополнительная защита во время разработки
+                                                                                              # (чтобы случайно не послать письма в время отладки рабочего)
+                                                                                              # рабочий шлёт письма только в том случае, если в таблице settings есть запись
+                                                                                              # с таким ключём и любым значением.
+                                     SETTINGS_FORCE_ALLOWED_KEY='post service force allowed', # Идентификатор настройки в таблице settings в админке Django
+                                                                                              # c перечнем уникальных идентификаторов сообщений, который будут отправляться
+                                                                                              # независимо от настройки SETTINGS_ALLOWED_KEY
+                                     MESSAGE_LIVE_TIME=2*24*60*60)                            # Время жизни запроса на отправку письма
