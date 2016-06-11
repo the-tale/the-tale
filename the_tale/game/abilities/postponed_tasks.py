3439910201db31b7db1a4f45f5e4b41dc38c55b6
Tiendil <a.eletsky@gmail.com>
2016-06-11 10:35:38 +0300

    gh-1946 Исключение при использовании пользовательской способности

--- the_tale/game/abilities/postponed_tasks.py
+++ the_tale/game/abilities/postponed_tasks.py
@@ -21,9 +21,14 @@ class UseAbilityTask(ComplexChangeTask):
         data = {}
 
         if self.data.get('building_id') is not None:
-            building = places_storage.buildings[self.data['building_id']]
-            data['building'] = { 'new_building_integrity': building.integrity,
-                                 'workers_to_full_repairing': building.workers_to_full_repairing }
+            building = places_storage.buildings.get(self.data['building_id'])
+
+            if building:
+                data['building'] = {'new_building_integrity': building.integrity,
+                                    'workers_to_full_repairing': building.workers_to_full_repairing }
+            else:
+                data['building'] = {'new_building_integrity': 0,
+                                    'workers_to_full_repairing': 0}
 
         if self.message:
             data['message'] = self.message
