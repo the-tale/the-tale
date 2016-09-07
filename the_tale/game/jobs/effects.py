c0bf427c3c26efe95062da4b8714bca5897b25b8
Tiendil <a.eletsky@gmail.com>
2016-09-07 22:47:26 +0300

    gh-1970 Проекты: эффекты проектов на параметры городов кончаются очень быстро

--- the_tale/game/jobs/effects.py
+++ the_tale/game/jobs/effects.py
@@ -85,7 +85,7 @@ class ChangePlaceAttribute(BaseEffect):
 
     def apply_positive(self, actor_type, actor_name, place, person, positive_heroes, negative_heroes, job_power):
         effect_value = self.base_value*job_power
-        effect_delta = effect_value * (1.0 / c.NORMAL_JOB_LENGTH)
+        effect_delta = effect_value * (1.0 / (24 * c.NORMAL_JOB_LENGTH))
         place.effects.add(effects.Effect(name=actor_name, attribute=self.attribute, value=effect_value, delta=effect_delta))
 
         return self.apply_to_heroes(actor_type=actor_type,
