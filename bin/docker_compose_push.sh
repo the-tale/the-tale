da925850ca141ad1743a11352fa075e502c2e70f
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2021-11-19 21:17:23 +0300

    загрузка контейнеров на github packages

---
+++ bin/docker_compose_push.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source ./bin/defaults.env
+
+docker-compose -f ./docker/docker-compose.build.yml \
+               --profile core \
+               --profile services \
+               --profile utils \
+               --profile site \
+               push $@
