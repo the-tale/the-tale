4cb737cd066775fde2fd58552763f600eda828c4
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2021-11-21 14:10:44 +0300

    доработки деплоя

--- bin/before_first_start.sh
+++ bin/before_first_start.sh
@@ -3,6 +3,8 @@
 ./bin/docker_compose.sh up -d
 ./bin/docker_compose.sh --profile services up -d
 
+./bin/docker_compose.sh run utils-site-migrations
+
 ./bin/docker_compose.sh run utils-site tt_django game_create_world
 ./bin/docker_compose.sh run utils-site tt_django accounts_create_superuser
 ./bin/docker_compose.sh run utils-site tt_django portal_postupdate_operations
