30659b3780cb959e372779e1d1deeee671bbb943
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2021-11-23 12:54:49 +0300

    упрощена инциализация базы

--- docker/utils_postgresql/bin/db_init.sh
+++ docker/utils_postgresql/bin/db_init.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
-# TODO: replace CREATEDB with NOCREATEDB for production
-
 tt_prepair_service_database () {
     echo "create user and database for '$1' with permissions: $2"
 
-    psql -q -v ON_ERROR_STOP=1 --username "$TT_DB_MANAGER_USER" --dbname "postgres" <<-EOSQL
+    psql -q -v ON_ERROR_STOP=1 --user "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
         CREATE USER $1 WITH PASSWORD '$1' $2;
 EOSQL
 
-    psql -q -v ON_ERROR_STOP=1 --username "$TT_DB_MANAGER_USER" --dbname "postgres" <<-EOSQL
-        GRANT $1 TO $TT_DB_MANAGER_USER;
-EOSQL
-
-    psql -q -v ON_ERROR_STOP=1 --username "$TT_DB_MANAGER_USER" --dbname "postgres" <<-EOSQL
+    psql -q -v ON_ERROR_STOP=1 --user "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
         CREATE DATABASE $1 WITH OWNER $1;
 EOSQL
 }
 
-export PGPASSWORD="$TT_DB_MANAGER_PASSWORD"
+
+export PGPASSWORD="$POSTGRES_PASSWORD"
 
 
 for name in $TT_DATABASES
