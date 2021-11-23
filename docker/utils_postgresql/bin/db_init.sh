f7e7b13ce7b08c179eb2508edcee47cf2641df9c
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2021-11-23 13:57:29 +0300

    доработки деплоя

--- docker/utils_postgresql/bin/db_init.sh
+++ docker/utils_postgresql/bin/db_init.sh
@@ -15,6 +15,9 @@ EOSQL
 
 export PGPASSWORD="$POSTGRES_PASSWORD"
 
+echo "========================="
+echo "Create services databases"
+echo "========================="
 
 for name in $TT_DATABASES
 do
@@ -26,4 +29,21 @@ do
     fi;
 
     tt_prepair_service_database "$name" "$permissions"
+done
+
+echo "==================="
+echo "Create datadog user"
+echo "==================="
+
+psql -q -v ON_ERROR_STOP=1 --user "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
+        CREATE USER $TT_DATADOG_POSTGRESQL_USER WITH PASSWORD '$TT_DATADOG_POSTGRESQL_PASSWORD'
+               NOSUPERUSER NOCREATEROLE NOINHERIT LOGIN NOREPLICATION NOCREATEDB;
+EOSQL
+
+for db_name in pg_stat_database pg_stat_user_tables pg_stat_user_indexes pg_stat_bgwriter pg_statio_user_tables;
+do
+    echo "grant SELECT on $db_name to $TT_DATADOG_POSTGRESQL_USER"
+    psql -q -v ON_ERROR_STOP=1 --user "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
+         grant SELECT ON $db_name to $TT_DATADOG_POSTGRESQL_USER
+EOSQL
 done
