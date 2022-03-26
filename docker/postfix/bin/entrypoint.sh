6adc7db0d124674bb0a0a51783e488a5a1a81bbd
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2022-03-26 21:19:28 +0100

    Починка отправки почты

--- docker/postfix/bin/entrypoint.sh
+++ docker/postfix/bin/entrypoint.sh
@@ -6,6 +6,9 @@ trap 'echo "\"${last_command}\" command failed with exit code $?."' ERR
 
 tt_render_configuration
 
+# TODO: move somewere else
+service rsyslog start
+
 postmap /etc/postfix/virtual
 
 exec "$@"
