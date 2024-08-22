2b54372546a292b84d7a582845f4441382033450
Aliaksei Yaletski <a.eletsky@gmail.com>
2024-08-22 16:32:53 +0200

    Сообщение о завершении игры (#2763)

--- docker/utils_postgresql/bin/db_init.sh
+++ docker/utils_postgresql/bin/db_init.sh
@@ -30,20 +30,3 @@ do
 
     tt_prepair_service_database "$name" "$permissions"
 done
-
-echo "==================="
-echo "Create datadog user"
-echo "==================="
-
-psql -q -v ON_ERROR_STOP=1 --user "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
-        CREATE USER $TT_DATADOG_POSTGRESQL_USER WITH PASSWORD '$TT_DATADOG_POSTGRESQL_PASSWORD'
-               NOSUPERUSER NOCREATEROLE NOINHERIT LOGIN NOREPLICATION NOCREATEDB;
-EOSQL
-
-for db_name in pg_stat_database pg_stat_user_tables pg_stat_user_indexes pg_stat_bgwriter pg_statio_user_tables;
-do
-    echo "grant SELECT on $db_name to $TT_DATADOG_POSTGRESQL_USER"
-    psql -q -v ON_ERROR_STOP=1 --user "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
-         grant SELECT ON $db_name to $TT_DATADOG_POSTGRESQL_USER
-EOSQL
-done
