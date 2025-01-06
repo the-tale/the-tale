773cf1cdcdf69f0cbf81d9d77f582e7de44067be
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2025-01-06 17:33:16 +0100

    backups improvements

--- docker/utils_postgresql/bin/entrypoint.sh
+++ docker/utils_postgresql/bin/entrypoint.sh
@@ -6,10 +6,11 @@ trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 export PGPASSWORD=$BACKUP_PASSWORD
 export PGUSER=$BACKUP_USER
-export PGHOST=core-postgresql
+export PGHOST=$POSTGRES_HOST
+export PGPORT=$POSTGRES_PORT
 
 export AWS_SHARED_CREDENTIALS_FILE=/root/aws.config
 
-wait-for-it $PGHOST:5432 -t $TT_WAIT_TIMEOUT
+wait-for-it $PGHOST:$PGPORT -t $TT_WAIT_TIMEOUT
 
 exec "$@"
